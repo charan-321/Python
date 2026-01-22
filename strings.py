fee2d7d64883f681f42185e216ea84213d947722
charan-321 <badnapuramchirucharantej@gmail.com>
2026-01-22 20:19:41 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,25 @@
+#s1="daygfulyfguly"
+#s2="daygfulyfguly"
+#print(s1);
+#print(s2);
+#print("day" in s1);
+#s3='this is a multi line string example for demonstration purpose print(s3);'
+#print(s3);
+#print(s3[0:6]);
+#print(s2[1:8]);
+#for char in s2:
+#   print(char)
+#for char in "hello world":
+#   print(char);
+#print(s2.lower());
+#print(s2.upper());
+#print(s2.replace("day","night"));#format is used to replace specific words from a given string
+#print("D"+s2[1:]);
+#strip()
+#a=" hiii "# used to remove spaces in the string
+#print(a.strip());
+#print(a.replace("hiii","hello"));
+#f-strings
+a=1;
+b="hi";
+print(f"a:{a},b:{b}");
