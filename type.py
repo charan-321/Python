052f38579263a6b64bbc666b4e6e4fccab2ff035
charan-321 <badnapuramchirucharantej@gmail.com>
2026-01-24 16:42:39 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,12 @@
+#a=10;
+#print(type(a));
+#b=str(a);
+#print(type(b));
+#a=input("enter a string:");
+#print(type(a));
+'''a=10;
+print(type(a));'''
+c=True;
+print(type(c));
+d=str(c);
+print(type(d));
