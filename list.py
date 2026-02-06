221ff689d22df5d7e08f25f0efa40d04402c5f98
charan-321 <badnapuramchirucharantej@gmail.com>
2026-02-06 20:30:45 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,44 @@
+# l=[1,2,3,"hiiiiiiiiiiiii",["12345678909",1,23,4,"be confident"],4.5,1+2j,True]
+# print(l)
+# print(type(l))
+# tuple_1=tuple(l)
+# print(tuple_1)
+# print(type(tuple_1))
+# l=[1,2,3,"hiiiiiiiiiiiii",["12345678909",1,23,4,"be confident"],4.5,1+2j,True]
+#print(l[2])
+#print(l[4][0])
+# a=l[4][2]
+# print(a)
+# a=["mommy",36,"daddy",52,"do not worry",True,False]
+# l.extend(a)
+# print(l)
+# a=[1,2,3,4,5]
+# a.append(5)
+# print(a)
+# a.insert(2,"hey")
+# # print(a)
+# a.insert(5,"hey")
+# print(a)
+# a.insert(6,3+1j)
+# print(a)
+# b=delete(a)
+# print(b)
+# print(a)
+# a=[1,2,3,4,5]
+# a.clear()
+# print(a)
+# a=[1,2,3,4,5]
+# a.remove(1)
+# print(a)
+# a.pop(2)
+# print(a)
+#l=[1,2,3,"hi",["12345678909",1,23,4,5],4.5,1+2j,True]
+#print(l)
+# print(len(l))
+# print(l[4][4])
+# print(l[4][0])
+list_1=[1,2,3,4,5]
+#print([i*i for i in list_1])
+#print([i*i for i in list_1 if i%2==0])
+#print(["even" if i%2==0 else "odd" for i in list_1])
+print([1 if i%2!=0 else 2 for i in list_1])
